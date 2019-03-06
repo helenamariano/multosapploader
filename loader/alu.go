385f6f96664a45def7cebdeb8d3b1b963dd7d5d0
Helena Mariano <helenamariano@gmail.com>
2019-03-06 15:48:27 +1100

    Add alu file parser functionality

---
+++ loader/alu.go
@@ -0,0 +1,98 @@
+package loader
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+const mcdNumberLength = 8
+const segmentSizeLength = 2
+const maxSegmentSize = math.MaxUint16
+
+// Alu is the Strucure of an Application Load Unit as defined
+// https://www.multos.com/uploads/GALU.pdf
+type Alu struct {
+	McdNumber []byte
+	Code      []byte
+	Data      []byte
+	Dir       []byte
+	Fci       []byte
+	Sig       []byte
+	Ktu       []byte
+}
+
+type errReader struct {
+	io.Reader
+	err error
+}
+
+func (e *errReader) readBytes(numBytes int) ([]byte, error) {
+	if e.err != nil {
+		return nil, e.err
+	}
+
+	bytes := make([]byte, numBytes)
+	var numBytesRead int
+	numBytesRead, e.err = e.Reader.Read(bytes)
+	if numBytes != numBytesRead {
+		errString := fmt.Sprintf("Expected to read %d bytes but acutally read %d", numBytes, numBytesRead)
+		e.err = errors.New(errString)
+		return nil, e.err
+	}
+	return bytes, e.err
+}
+
+func (e *errReader) readSegment() ([]byte, error) {
+	if e.err != nil {
+		return nil, e.err
+	}
+
+	segmentSizeBytes, _ := e.readBytes(segmentSizeLength)
+	if e.err != nil {
+		return nil, e.err
+	}
+	segmentSize := int(binary.BigEndian.Uint16(segmentSizeBytes))
+	segment, _ := e.readBytes(segmentSize)
+	return segment, e.err
+}
+
+// ParseAlu reads an ALU file and returs an Alc structure
+func ParseAlu(filename string) (*Alu, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	reader = bufio.NewReaderSize(reader, maxSegmentSize)
+	er := errReader{Reader: reader}
+
+	mcdNumber, _ := er.readBytes(mcdNumberLength)
+	code, _ := er.readSegment()
+	data, _ := er.readSegment()
+	dir, _ := er.readSegment()
+	fci, _ := er.readSegment()
+	sig, _ := er.readSegment()
+	ktu, _ := er.readSegment()
+
+	if er.err != nil {
+		return nil, er.err
+	}
+
+	alu := Alu{
+		McdNumber: mcdNumber,
+		Code:      code,
+		Data:      data,
+		Dir:       dir,
+		Fci:       fci,
+		Sig:       sig,
+		Ktu:       ktu,
+	}
+	return &alu, er.err
+}
---
+++ loader/alu_test.go
@@ -0,0 +1,48 @@
+package loader
+
+import (
+	"testing"
+
+	"github.com/go-test/deep"
+)
+
+// expected strucutre from testfiles/sample.alu
+// generated from https://github.com/helenamariano/multos-sample-app
+var sampleAlu = Alu{
+	McdNumber: []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	Code:      []byte{0x0F, 0x00, 0xB9, 0x01, 0x00, 0x00, 0x39, 0x01, 0x00, 0x00, 0x47, 0x01, 0xFF, 0xFF, 0x04, 0x04},
+	Data:      []byte{0x00},
+	Dir:       []byte{0x61, 0x0C, 0x4F, 0x01, 0x11, 0x50, 0x07, 0x59, 0x6F, 0x74, 0x69, 0x4B, 0x65, 0x79},
+	Fci:       []byte{0x11, 0x22, 0x33},
+	Sig:       []byte{},
+	Ktu:       []byte{},
+}
+
+func TestParseAlu(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Alu
+		wantErr bool
+	}{
+		{"Valid ALU", args{filename: "testfiles/sample.alu"}, &sampleAlu, false},
+		{"Missing data segment", args{filename: "testfiles/sample_missing_data_segment.alu"}, nil, true},
+		{"Incomplete code segment", args{filename: "testfiles/sample_incomplete_code_segment.alu"}, nil, true},
+		{"File does not exist", args{filename: "fileDoesNotExit"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseAlu(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseAlu() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if diff := deep.Equal(tt.want, got); diff != nil {
+				t.Error(diff)
+			}
+		})
+	}
+}
